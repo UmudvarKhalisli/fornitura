23c2dd272b4ea9a4aee614c542b12099cb7c8aaa
Umudvar <umudvarkhalisli@gmail.com>
2026-05-16 03:30:29 -0700

    perf: add loading.tsx for instant page transitions

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,9 @@
+import { Loading } from '@/components/shared/loading';
+
+export default function RootLoading() {
+  return (
+    <div className="flex items-center justify-center min-h-[60vh]">
+      <Loading />
+    </div>
+  );
+}
