6de8065ab6ab03892887ef38804ebd73cab01e28
Umudvar <umudvarkhalisli@gmail.com>
2026-05-18 05:12:54 -0700

    Refactor Google Analytics to use env variable NEXT_PUBLIC_GA_ID

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -85,14 +85,14 @@ export default async function LocaleLayout({
         />
         <ScrollToTop />
         <OrganizationSchema />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VP03YFHTSV"></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-VP03YFHTSV');
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
             `,
           }}
         />
